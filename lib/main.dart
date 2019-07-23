e5de215d708bcf58611447790a4e814cca7991b3
2277Dhruv <dp4patel@uwaterloo.ca>
2019-07-23 15:17:36 -0400

    Implemented sign up/sign in - does error handling for the most part and has the dialog box and all

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:Dime/EditCardsScreen.dart';
+import 'package:Dime/login.dart';
 import 'package:Dime/socialPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'homePage.dart';
 import 'loginPage.dart';
 import 'onboarding.dart';
-
+import 'signup.dart';
 
 
 void main() => runApp(Dime());
@@ -16,7 +17,7 @@ class Dime extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Dime",
-      home: ScrollPage(),
+      home: SignupPage(),
       // routes: <String, WidgetBuilder>{
       //   '/homepage': (BuildContext context) => new MyHomePage(),
       //   '/loginpage': (BuildContext context) => new LoginPage(),

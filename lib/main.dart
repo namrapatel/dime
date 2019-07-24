b3e691d6361b61a645856160de575df0a6e7fb6a
Taher <t.ankleshwaria@gmail.com>
2019-07-23 18:24:40 -0600

    Revert "Merge pull request #6 from namrapatel/CardEditScreensUI"
    This reverts commit 8b2a1bdd135ca6fed3852b76960ec94a4b7fa9dc, reversing
    changes made to 4c30c5235feb758accd3ea783fd06a84c03c56b8.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:Dime/EditCardsScreen.dart';
-import 'package:Dime/profileScreen.dart';
 import 'package:Dime/login.dart';
 import 'package:Dime/socialPage.dart';
 import 'package:flutter/material.dart';
@@ -7,8 +6,8 @@ import 'package:flutter/cupertino.dart';
 import 'homePage.dart';
 import 'loginPage.dart';
 import 'onboarding.dart';
-import 'profileScreen.dart';
 import 'signup.dart';
+
 
 void main() => runApp(Dime());
 

b84c959ce34b9c65ef171cb60f50cdb5f315f9c1
shahab <shahab.salem6@gmail.com>
2019-08-12 18:04:31 -0600

    Revert "chat UI"
    This reverts commit d7cd063582617df974c1e3e465f3400755768507.

--- lib/chatList.dart
+++ lib/chatList.dart
@@ -5,7 +5,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import 'package:page_transition/page_transition.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 
 final screenH = ScreenUtil.instance.setHeight;
 final screenW = ScreenUtil.instance.setWidth;
@@ -138,42 +137,141 @@ class _ChatListState extends State<ChatList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF1458EA),
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-                        icon: Icon(
-                          Icons.arrow_back_ios,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-        title: Text(
-                      "Messages",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold),
-                    ),
-      ),
+      //backgroundColor: Color(0xFFECE9E4),
       body: ListView(
         padding: EdgeInsets.all(0.0),
-        physics: BouncingScrollPhysics(),
+        physics: NeverScrollableScrollPhysics(),
         children: <Widget>[
           Column(
             children: <Widget>[
               SizedBox(height: screenH(20)),
+              Stack(
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 5,
+                    //color: Color(0xFFECE9E4),
+                  ),
+                  Positioned(
+                      top: MediaQuery.of(context).size.height / 55,
+                      left: MediaQuery.of(context).size.width / 55,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      )),
+                  Positioned(
+                    top: MediaQuery.of(context).size.height / 10,
+                    left: MediaQuery.of(context).size.width / 22,
+                    child: Text(
+                      "Messages",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
               Container(
                   width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
+                  height: MediaQuery.of(context).size.height / 1.2,
                   child: Column(
                     children: <Widget>[
                       Column(
                         children: <Widget>[buildMessages()],
                       )
 
+//                      FutureBuilder<List<MessageTile>>(
+//                      future: getMessages(),
+//                      builder: (context, snapshot) {
+//                      if (!snapshot.hasData)
+//                        return Center(
+//                            child: Container(
+//
+//                              child: Column(
+//                                children: <Widget>[
+//                                  Padding(
+//                                    padding: EdgeInsets.symmetric(
+//                                        vertical: (50.0)),
+//                                    child: Container(
+//                                        width: (100),
+//                                        child: Text(
+//                                          "You currently have no messages",
+//                                          textAlign: TextAlign.center,
+//                                        )),
+//                                  ),
+//                                ],
+//                              ),
+//                            ));
+//
+//                      return Column(
+//                        children:
+//                          snapshot.data
+//
+//                      );
+//                      }),
+
+//                           StreamBuilder<QuerySnapshot>(
+//                          stream: Firestore.instance
+//                              .collection('chatMessages').where('to',isEqualTo: currentUserModel.uid).orderBy('timestamp',descending: true).snapshots(),
+//                          builder: (context, snapshot) {
+//                            if (!snapshot.hasData ||
+//                                snapshot.data.documents.length == 0) {
+//                              return Center(
+//                                  child: Container(
+//
+//                                    child: Column(
+//                                      children: <Widget>[
+//                                        Padding(
+//                                          padding: EdgeInsets.symmetric(
+//                                              vertical: (50.0)),
+//                                          child: Container(
+//                                              width: (100),
+//                                              child: Text(
+//                                                "You currently have no messages",
+//                                                textAlign: TextAlign.center,
+//                                              )),
+//                                        ),
+//                                      ],
+//                                    ),
+//                                  ));
+//                            }
+//                            final docs = snapshot.data.documents;
+//                            List<MessageTile> messageTiles = [];
+//                            for (var doc in docs) {
+//                              String text= doc.data['text'];
+//                              String photo=doc.data['receiverPhoto'];
+//                              String name=doc.data['receiverName'];
+//                              String from=doc.data['from'];
+//                                  String to=doc.data['to'];
+//                              var storedDate = doc.data['timestamp'];
+//
+//                              String elapsedTime =
+//                              timeago.format(storedDate.toDate());
+//                              String timestamp = '$elapsedTime';
+//
+//                              messageTiles.add(new MessageTile(text: text,from: from,timestamp: timestamp,senderName:name,senderPhoto: photo,to:to));
+//                            }
+//
+//                            return Container(
+//                              child: ListView.separated(
+//                                itemCount: messageTiles.length,scrollDirection: Axis.vertical,shrinkWrap: true,
+//                                itemBuilder: (context, index) {
+//                                  return messageTiles[index];
+//                                },
+//
+//                                separatorBuilder: (context, index) {
+//                                  return Divider();
+//                                },
+//                              ),
+//
+//                            );
+//                          }),
                     ],
                   )),
             ],
@@ -228,7 +326,7 @@ class MessageTile extends StatelessWidget {
       subtitle: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Text(text.length >= 39 ? text.substring(0, 39) + "..." : text),
+          Text(text.length >= 39 ? text.substring(0, 39) : text),
         ],
       ),
       //MAX OF 40 CHARACTERS BEFORE "..."

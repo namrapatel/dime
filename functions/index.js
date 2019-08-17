1b225cd6ea58b11fa006f4c7c2ec426c69ec3975
Mahad Zaryab <mahadzaryab1@gmail.com>
2019-08-17 09:14:57 -0400

    Fixed notifications so they're not sent if you comment on your own post or send a message to yourself

--- functions/index.js
+++ functions/index.js
@@ -4,13 +4,12 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 var msgData;
-var fromId = "test";
-var toId = "test";
-var fromName = "test";
+var fromId = "";
+var toId = "";
+var fromName = "";
 var sendTokens = [];
 var payLoad = {};
-var tempToken = ["dX4lKMAvq_Q:APA91bFY_hCFMgRQIxV334etgAlxIBFLmt5Tc5GkFG9ROi9MUx-DKVcmHp5abMt3cM8J8EzEOsajg_nCd5Z_NLBZUHHQ7splFA3FUrP6C1GSDYQWuMjemCRz16UdgQ4mVPT6Gbpcxh0g"];
-var commenterName = "test";
+var commenterName = "";
 exports.chatTrigger = functions.firestore.document('notifMessages/{messageId}').onCreate((snapshot, context) => {
     msgData = snapshot.data();
     fromId = msgData.from;
@@ -19,33 +18,35 @@ exports.chatTrigger = functions.firestore.document('notifMessages/{messageId}').
     console.log(fromId);
     console.log(toId);
 
-    admin.firestore().collection('users').doc(toId).get().then(function(doc) {
-        sendTokens = doc.data().tokens;
-        console.log(sendTokens);
-        admin.firestore().collection('users').doc(fromId).get().then(function(doc) {
-            fromName = doc.data().displayName;
-            payLoad = {
-                "notification": {
-                    "title": fromName,
-                    "body": msgData.text,
-                    "sound": "default"
-                },
-                "data": {
-                    "click_action": "FLUTTER_NOTIFICATION_CLICK",
-                     "title": fromName,
-                     "body": msgData.text,
-                    "senderId": fromId,
-                    "notifType": "chat"
+    if (fromId !== toId) {
+        admin.firestore().collection('users').doc(toId).get().then(function(doc) {
+            sendTokens = doc.data().tokens;
+            console.log(sendTokens);
+            admin.firestore().collection('users').doc(fromId).get().then(function(doc) {
+                fromName = doc.data().displayName;
+                payLoad = {
+                    "notification": {
+                        "title": fromName,
+                        "body": msgData.text,
+                        "sound": "default"
+                    },
+                    "data": {
+                        "click_action": "FLUTTER_NOTIFICATION_CLICK",
+                         "title": fromName,
+                         "body": msgData.text,
+                        "senderId": fromId,
+                        "notifType": "chat"
+                    }
                 }
-            }
-            console.log(fromName);
-            return admin.messaging().sendToDevice(sendTokens, payLoad).then((response) => {
-                console.log('Pushed them all');
-            }).catch((err) => {
-                console.log(err);
+                console.log(fromName);
+                return admin.messaging().sendToDevice(sendTokens, payLoad).then((response) => {
+                    console.log('Pushed them all');
+                }).catch((err) => {
+                    console.log(err);
+                });
             });
         });
-    });
+    }
 });
 
 exports.postNotifTrigger = functions.firestore.document('postNotifs/{notifId}').onCreate((snapshot, context) => {
@@ -54,52 +55,33 @@ exports.postNotifTrigger = functions.firestore.document('postNotifs/{notifId}').
     ownerId = notifData.ownerId;
     commenterName = notifData.commenterName;
 
-    admin.firestore().collection('users').doc(ownerId).get().then(function(doc) {
-        sendTokens = doc.data().tokens;
-        admin.firestore().collection('users').doc(commenterId).get().then(function(doc) {
-            fromName = commenterName;
-            payLoad = {
-                "notification": {
-                    "title": "Your post has a new comment from " + fromName,
-                    "body": notifData.text,
-                    "sound": "default"
-                },
-                "data": {
-                    "click_action": "FLUTTER_NOTIFICATION_CLICK",
-                    "title": "Your post has a new comment from " + fromName,
-                    "body": notifData.text,
-                    "notifType": "postNotif",
-                    "postId": notifData.postID,
-                    "type": notifData.type
+    if (commenterId !== ownerId) {
+        admin.firestore().collection('users').doc(ownerId).get().then(function(doc) {
+            sendTokens = doc.data().tokens;
+            admin.firestore().collection('users').doc(commenterId).get().then(function(doc) {
+                fromName = commenterName;
+                payLoad = {
+                    "notification": {
+                        "title": "Your post has a new comment from " + fromName,
+                        "body": notifData.text,
+                        "sound": "default"
+                    },
+                    "data": {
+                        "click_action": "FLUTTER_NOTIFICATION_CLICK",
+                        "title": "Your post has a new comment from " + fromName,
+                        "body": notifData.text,
+                        "notifType": "postNotif",
+                        "postId": notifData.postID,
+                        "type": notifData.type
+                    }
                 }
-            }
-            console.log(payLoad)
-            return admin.messaging().sendToDevice(sendTokens, payLoad).then((response) => {
-                console.log('Pushed them all');
-            }).catch((err) => {
-                console.log(err);
+                console.log(payLoad)
+                return admin.messaging().sendToDevice(sendTokens, payLoad).then((response) => {
+                    console.log('Pushed them all');
+                }).catch((err) => {
+                    console.log(err);
+                });
             });
         });
-    });
+    }
 });
-
-    // payLoad = {
-    //     "notification": {
-    //         "title": "New message from " + fromName,
-    //         "body": "test",
-    //         "sound": "default"
-    //     },
-    //     "data": {
-    //         "senderName": fromName,
-    //         "message": "test"
-    //     }
-    // }
-    // console.log(fromName);
-    // console.log(sendTokens);
-
-    // return admin.messaging().sendToDevice("eRc5HvOhUVo:APA91bEkJHQF4hJcBUH8QqGTjg0IQ_GeEJpIvL5EnfqW7sHWURYC7gNmPUY-uOLJMvH5ysMzBYTsKnvliWszXyFdgEUMMcbAhh7aEbaHYNwxNNYpmjrvK20FJmdvq0go4QB044yJhDkh", payLoad).then((response) => {
-    //     console.log('Pushed them all');
-    // }).catch((err) => {
-    //     console.log(err);
-    //"eRc5HvOhUVo:APA91bEkJHQF4hJcBUH8QqGTjg0IQ_GeEJpIvL5EnfqW7sHWURYC7gNmPUY-uOLJMvH5ysMzBYTsKnvliWszXyFdgEUMMcbAhh7aEbaHYNwxNNYpmjrvK20FJmdvq0go4QB044yJhDkh"
-    // });

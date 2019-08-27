94b65a2d8170e3fd904f4ad07b374dc0c747e427
2277Dhruv <dp4patel@uwaterloo.ca>
2019-08-27 15:15:26 -0600

    Android Production Release

--- gen/com/dime/inc/Manifest.java
+++ gen/com/dime/inc/Manifest.java
@@ -1,6 +1,6 @@
 /*___Generated_by_IDEA___*/
 
-package com.dime.inc;
+package com.dime2.inc;
 
 /* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
 public final class Manifest {

c8fc010388a5e73922e7ce7f2faf8e251f783920
2277Dhruv <dp4patel@uwaterloo.ca>
2019-08-30 17:36:23 -0600

    i tried

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -20,8 +20,14 @@ String twitter;
 String linkedIn;
 String github;
 
-class onBoarding extends StatelessWidget {
+class onBoarding extends StatefulWidget {
   @override
+  _onBoardingState createState() => _onBoardingState();
+
+}
+
+class _onBoardingState extends State<onBoarding> {
+  @override 
   Widget build(BuildContext context) {
     double defaultScreenWidth = 414.0;
     double defaultScreenHeight = 896.0;
@@ -30,173 +36,164 @@ class onBoarding extends StatelessWidget {
       height: defaultScreenHeight,
       allowFontScaling: true,
     )..init(context);
-    return WillPopScope(
-      onWillPop: () {
-        print('hello');
-      },
-      child: Scaffold(
-        //title of app
-
-        body: Builder(
-          builder: (context) => IntroViewsFlutter(
-                [
-                  PageViewModel(
-                    pageColor: const Color(0xFF1458EA),
-                    body: Padding(
-                      padding: EdgeInsets.all(screenH(8.0)),
-                      child: Text(
-                        'Find people around your location, read their interests, and chat or connect with their medias!',
-                        style: TextStyle(
-                            fontFamily: 'futura',
-                            color: Colors.white,
-                            fontSize: screenF(18.0)),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    title: Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                      child: Text('People around you',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: screenF(36.0),
-                              // fontWeight: FontWeight.bold,
-                              fontFamily: 'futura')),
-                    ),
-                    mainImage: Image.asset(
-                      'assets/darkpeoplearoundyou.png',
-                      height: screenH(600.0),
-                      width: screenW(600.0),
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                  PageViewModel(
-                    pageColor: const Color(0xFF1458EA),
-                    body: Padding(
-                      padding: EdgeInsets.all(screenH(8.0)),
-                      child: Text(
-                        'All your online media handles and a small glimpse into who you are are easily accessible through your personal cards.',
-                        style: TextStyle(
-                            fontFamily: 'futura',
-                            color: Colors.white,
-                            fontSize: screenF(18.0)),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    title: Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                      child: Text('Clickable Cards',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: screenF(36.0),
-                              // fontWeight: FontWeight.bold,
-                              fontFamily: 'futura')),
-                    ),
-                    mainImage: Image.asset(
-                      'assets/clickablecards.png',
-                      height: screenH(600.0),
-                      width: screenW(600.0),
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                  PageViewModel(
-                      pageColor: Colors.white,
-                      body: OnboardingSave(),
-                      title: Padding(
-                        padding: EdgeInsets.only(top: screenH(22.0)),
-                        child: Column(
-                          children: <Widget>[
-                            Text('Enter your handles!',
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: screenF(36.0),
-                                    // fontWeight: FontWeight.bold,
-                                    fontFamily: 'futura')),
-                            SizedBox(
-                              height: screenH(2.5),
-                            ),
-                            Text(
-                              'Handles are clickable on cards',
-                              style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: screenF(16.0),
-                                  // fontWeight: FontWeight.bold,
-                                  fontFamily: 'futura'),
-                            )
-                          ],
-                        ),
-                      ),
-                      mainImage: MediaForm()),
-                  PageViewModel(
-                    pageColor: const Color(0xFF1458EA),
-                    body: Padding(
-                      padding: EdgeInsets.all(screenH(8.0)),
-                      child: Text(
-                        'A full list of the people who go to your university, put faces to names.',
-                        style: TextStyle(
-                            fontFamily: 'futura',
-                            color: Colors.white,
-                            fontSize: screenF(18.0)),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    title: Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                      child: Text('University Directory',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: screenF(36.0),
-                              // fontWeight: FontWeight.bold,
-                              fontFamily: 'futura')),
-                    ),
-                    mainImage: Image.asset(
-                      'assets/darkdirectory.png',
-                      height: screenH(600.0),
-                      width: screenW(600.0),
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                  PageViewModel(
-                    pageColor: const Color(0xFF1458EA),
-                    body: Padding(
-                      padding: EdgeInsets.all(screenH(8.0)),
-                      child: Text(
-                        "We're constantly working to make your experience on our platform as seamless as possible. Stay tuned for new features!",
-                        style: TextStyle(
-                            fontFamily: 'futura',
-                            color: Colors.white,
-                            fontSize: screenF(18.0)),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    title: Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                      child: Text('More to come',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: screenF(36.0),
-                              // fontWeight: FontWeight.bold,
-                              fontFamily: 'futura')),
-                    ),
-                    mainImage: Image.asset(
-                      'assets/improvingDrawing.png',
-                      height: screenH(600.0),
-                      width: screenW(600.0),
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                ],
-                showSkipButton: false,
-                onTapDoneButton: () {
-                  Navigator.push(context,
-                      CupertinoPageRoute(builder: (context) => Profile()));
-                },
-                pageButtonTextStyles: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
+    return Scaffold(
+      body: Builder(
+        builder: (context) => IntroViewsFlutter(
+          [
+            PageViewModel(
+              
+              pageColor: const Color(0xFF1458EA),
+              body: Padding(
+                padding: EdgeInsets.all(screenH(8.0)),
+                child: Text(
+                  'Find people around your location, read their interests, and chat or connect with their medias!',
+                  style: TextStyle(
+                      fontFamily: 'futura',
+                      color: Colors.white,
+                      fontSize: screenF(18.0)),
+                  textAlign: TextAlign.center,
                 ),
-              ), //IntroViewsFlutter
-        ), //Builder
-      ),
+              ),
+              title: Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Text('People around you',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenF(36.0),
+                        // fontWeight: FontWeight.bold,
+                        fontFamily: 'futura')),
+              ),
+              mainImage: Image.asset(
+                'assets/darkpeoplearoundyou.png',
+                height: screenH(600.0),
+                width: screenW(600.0),
+                alignment: Alignment.center,
+              ),
+            ),
+            PageViewModel(
+              pageColor: const Color(0xFF1458EA),
+              body: Padding(
+                padding: EdgeInsets.all(screenH(8.0)),
+                child: Text(
+                  'All your online media handles and a small glimpse into who you are are easily accessible through your personal cards.',
+                  style: TextStyle(
+                      fontFamily: 'futura',
+                      color: Colors.white,
+                      fontSize: screenF(18.0)),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              title: Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Text('Clickable Cards',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenF(36.0),
+                        // fontWeight: FontWeight.bold,
+                        fontFamily: 'futura')),
+              ),
+              mainImage: Image.asset(
+                'assets/clickablecards.png',
+                height: screenH(600.0),
+                width: screenW(600.0),
+                alignment: Alignment.center,
+              ),
+            ),
+            PageViewModel(
+                pageColor: Color(0xFFf5f4f2),
+                body: OnboardingSave(),
+                title: Column(
+                  children: <Widget>[
+                    Text('Enter your handles!',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: screenF(30.0),
+                            // fontWeight: FontWeight.bold,
+                            fontFamily: 'futura')),
+                    // SizedBox(
+                    //   height: screenH(1.5),
+                    // ),
+                    Text(
+                      'Handles are clickable on cards',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: screenF(16.0),
+                          // fontWeight: FontWeight.bold,
+                          fontFamily: 'futura'),
+                    )
+                  ],
+                ),
+                mainImage: MediaForm()),
+            PageViewModel(
+              pageColor: const Color(0xFF1458EA),
+              body: Padding(
+                padding: EdgeInsets.all(screenH(8.0)),
+                child: Text(
+                  'A full list of the people who go to your university, put faces to names.',
+                  style: TextStyle(
+                      fontFamily: 'futura',
+                      color: Colors.white,
+                      fontSize: screenF(18.0)),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              title: Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Text('University Directory',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenF(36.0),
+                        // fontWeight: FontWeight.bold,
+                        fontFamily: 'futura')),
+              ),
+              mainImage: Image.asset(
+                'assets/darkdirectory.png',
+                height: screenH(600.0),
+                width: screenW(600.0),
+                alignment: Alignment.center,
+              ),
+            ),
+            PageViewModel(
+              pageColor: const Color(0xFF1458EA),
+              body: Padding(
+                padding: EdgeInsets.all(screenH(8.0)),
+                child: Text(
+                  "We're constantly working to make your experience on our platform as seamless as possible. Stay tuned for new features!",
+                  style: TextStyle(
+                      fontFamily: 'futura',
+                      color: Colors.white,
+                      fontSize: screenF(18.0)),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              title: Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Text('More to come',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenF(36.0),
+                        // fontWeight: FontWeight.bold,
+                        fontFamily: 'futura')),
+              ),
+              mainImage: Image.asset(
+                'assets/improvingDrawing.png',
+                height: screenH(600.0),
+                width: screenW(600.0),
+                alignment: Alignment.center,
+              ),
+            ),
+          ],
+          showSkipButton: false,
+          onTapDoneButton: () {
+            Navigator.push(
+                context, CupertinoPageRoute(builder: (context) => Profile()));
+          },
+          pageButtonTextStyles: TextStyle(
+            color: Colors.white,
+            fontSize: 18.0,
+          ),
+        ), //IntroViewsFlutter
+      ), //Builder
     ); //Material App
   }
 }
@@ -247,9 +244,11 @@ class _MediaFormState extends State<MediaForm> {
                     ),
                   ))),
         ),
+
         SizedBox(
           height: screenH(10.0),
         ),
+
         Container(
           width: screenW(370),
           child: TextField(
@@ -288,9 +287,11 @@ class _MediaFormState extends State<MediaForm> {
             ),
           ),
         ),
+
         SizedBox(
           height: screenH(10.0),
         ),
+
         Container(
           width: screenW(370),
           child: TextField(
@@ -329,9 +330,11 @@ class _MediaFormState extends State<MediaForm> {
             ),
           ),
         ),
+
         SizedBox(
           height: screenH(10.0),
         ),
+
         Container(
           width: screenW(370),
           child: TextField(
@@ -370,9 +373,11 @@ class _MediaFormState extends State<MediaForm> {
             ),
           ),
         ),
+
         SizedBox(
           height: screenH(10.0),
         ),
+
         Container(
           width: screenW(370),
           child: TextField(
@@ -413,62 +418,119 @@ class _MediaFormState extends State<MediaForm> {
         ),
 
         // Container(
+
         //   width: screenW(250),
+
         //   height: screenH(60),
+
         //   child: FloatingActionButton(
+
         //     shape: RoundedRectangleBorder(
+
         //         borderRadius: BorderRadius.all(Radius.circular(16.0))),
+
         //     elevation: screenH(5),
+
         //     onPressed: () {
+
         //       updateSocialCard();
+
         //       Flushbar(
+
         //         //  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+
         //         borderRadius: 15,
+
         //         messageText: Padding(
+
         //           padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+
         //           child: Column(
+
         //             mainAxisAlignment: MainAxisAlignment.start,
+
         //             crossAxisAlignment: CrossAxisAlignment.start,
+
         //             children: <Widget>[
+
         //               Text(
+
         //                 'Saved!',
+
         //                 style: TextStyle(
+
         //                     color: Colors.black, fontWeight: FontWeight.bold),
+
         //               ),
+
         //               Text(
+
         //                 'Swipe right! Your cards have now been updated.',
+
         //                 style: TextStyle(color: Colors.grey),
+
         //               )
+
         //             ],
+
         //           ),
+
         //         ),
+
         //         backgroundColor: Colors.white,
+
         //         boxShadows: [
+
         //           BoxShadow(
+
         //               color: Colors.black12.withOpacity(0.1),
+
         //               blurRadius: (15),
+
         //               spreadRadius: (5),
+
         //               offset: Offset(0, 3)),
+
         //         ],
+
         //         flushbarPosition: FlushbarPosition.TOP,
+
         //         icon: Padding(
+
         //           padding: EdgeInsets.fromLTRB(15, 8, 8, 8),
+
         //           child: Icon(
+
         //             Icons.save_alt,
+
         //             size: 28.0,
+
         //             color: Color(0xFF1458EA),
+
         //           ),
+
         //         ),
+
         //         duration: Duration(seconds: 3),
+
         //       )..show(context);
+
         //     },
+
         //     backgroundColor: Color(0xFF1458EA),
+
         //     child: Text(
+
         //       "Save",
+
         //       style: TextStyle(fontSize: screenF(20), color: Colors.white),
+
         //     ),
+
         //   ),
+
         // )
+        
       ],
     );
   }

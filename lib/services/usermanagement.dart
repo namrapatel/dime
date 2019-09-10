2562fd778b57ebf63f606b6cf113985553defd86
shahab <shahab.salem6@gmail.com>
2019-09-10 16:18:17 -0600

    new user instead of no dispaly

--- lib/services/usermanagement.dart
+++ lib/services/usermanagement.dart
@@ -8,7 +8,7 @@ class UserManagement {
   storeNewUser(user, context) {
     FirebaseUser current = user;
     String uid = current.uid;
-    String defaultName = "No Display Name";
+    String defaultName = "New User";
     String defaultImage =
         'https://firebasestorage.googleapis.com/v0/b/dime-87d60.appspot.com/o/defaultprofile.png?alt=media&token=8cd5318b-9593-4837-a9f9-2a22c87463ef';
     Firestore.instance.collection('users').document('$uid').setData({

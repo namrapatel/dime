7b4e21b6650281c421aee54a89774879760d5176
Mahad Zaryab <mahadzaryab1@gmail.com>
2019-09-15 23:22:05 -0400

    minor fixes pt.2

--- lib/explore.dart
+++ lib/explore.dart
@@ -193,7 +193,7 @@ class _ExploreState extends State<Explore> {
                                 .document(currentUserModel.uid)
                                 .setData({
                               'likeType': 'social',
-                              'liked': true,
+                              'liked': false,
                               'timestamp': Timestamp.now(),
                               'unread': false
                             });
@@ -270,7 +270,7 @@ class _ExploreState extends State<Explore> {
                                 .document(currentUserModel.uid)
                                 .setData({
                               'likeType': 'prof',
-                              'liked': true,
+                              'liked': false,
                               'timestamp': Timestamp.now(),
                               'unread': false
                             });

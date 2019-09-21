ef5d6a3ef280c2a9b935009cc5453edb8bdac1f7
Taher <t.ankleshwaria@gmail.com>
2019-09-21 10:47:18 -0700

    Like button on UserCard.dart

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/namrapatel/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/namrapatel/Dime1.0"
-export "FLUTTER_TARGET=/Users/namrapatel/Dime1.0/lib/main.dart"
+export "FLUTTER_ROOT=/Users/mainuser/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mainuser/Dime1.0"
+export "FLUTTER_TARGET=/Users/mainuser/Dime1.0/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/namrapatel/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/mainuser/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=3"
 export "TRACK_WIDGET_CREATION=true"
